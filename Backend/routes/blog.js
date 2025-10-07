b8dc6f354b8d99fe39949b9edee73cc085bd3143
Saikat Pradhan <saikatpradhan65@gmail.com>
2025-10-07 12:11:08 +0530

    Okk

--- Backend/routes/blog.js
+++ Backend/routes/blog.js
@@ -2,6 +2,11 @@ const express = require("express");
 const route = express.Router();
 const { getAllBlogs, getBlog, addBlog } = require("../controller/blog");
 
+// Just for awake backend
+route.get("/ping", (req, res) => {
+  res.status(200).send("pong");
+});
+
 route.get("/", getAllBlogs);
 route.get("/:id", getBlog);
 route.post("/", addBlog);
