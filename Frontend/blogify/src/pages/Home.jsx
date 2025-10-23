2cf9f7381272eef5b5bc91abf4ea0457cfba129f
Saikat Pradhan <saikatpradhan65@gmail.com>
2025-10-23 19:17:19 +0530

    Done

--- Frontend/blogify/src/pages/Home.jsx
+++ Frontend/blogify/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className='flex justify-center items-center h-screen bg-amber-200 overflow-scroll dark:bg-neutral-800'>
+    <div className='flex justify-center items-center pt-1 lg:pt-0 h-screen bg-amber-200 overflow-scroll dark:bg-neutral-800'>
       <BlogItems />
     </div>
   );
